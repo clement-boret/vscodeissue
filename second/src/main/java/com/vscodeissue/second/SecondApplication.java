ae1583057c5eebce2d534ab5e08226fedf1d02cd
clement.boret <clement.boret@tidal.com>
2020-10-21 15:57:59 +0200

    last

--- second/src/main/java/com/vscodeissue/second/SecondApplication.java
+++ second/src/main/java/com/vscodeissue/second/SecondApplication.java
@@ -1,13 +1,17 @@
 package com.vscodeissue.second;
 
+import com.vscodeissue.first.MySharedEnum;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecondApplication {
 
+	private MySharedEnum vsCode = MySharedEnum.VSCODE;
 	public static void main(String[] args) {
 		SpringApplication.run(SecondApplication.class, args);
+
 	}
 
 }
